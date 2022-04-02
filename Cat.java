a69c2073fc7b783224ec12477884e3445b70f733
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-02 17:58:59 +0300

    cat class

--- Cat.java
+++
@@ -1,8 +0,0 @@
-public class Cat extends Feline {
-	public void makeNoise(){
-		System.out.println("Cat Noises all the Way");
-	}
-	public void eat(){
-		System.out.println("Cat drinks milk");
-	}
-}
