3f4f5603c015fc47aa42c7236fe47959dcbb9e30
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-02 17:34:53 +0300

    This is the abstract cat class

---
+++ Cat.java
@@ -0,0 +1,8 @@
+public class Cat extends Feline {
+	public void makeNoise(){
+		System.out.println("Cat Noises all the Way");
+	}
+	public void eat(){
+		System.out.println("Cat drinks milk");
+	}
+}
