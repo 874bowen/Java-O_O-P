cee5affa34ec0956300d475d2329c4fdf63f0db6
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-05 06:38:39 +0300

    Abstract class animal

---
+++ Anime/Animal.java
@@ -0,0 +1,8 @@
+public abstract class Animal {
+	public void breath(){
+		System.out.println("I do breath yowh!!");
+	}
+	public abstract void eat();
+	public abstract void exercise();
+	public abstract void sleep();
+}
